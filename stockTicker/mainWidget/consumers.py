e913ffc21f95b755919373d2eca35dc9182cdebf
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2022-01-15 09:30:29 -0500

    Update consumers.py

--- stockTicker/mainWidget/consumers.py
+++ stockTicker/mainWidget/consumers.py
@@ -2,10 +2,10 @@
 import json
 from urllib.parse import parse_qs
 from asgiref.sync import sync_to_async, async_to_sync
-
+from mainWidget.models import StockDetail
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
-
+import copy
 
 class StockConsumer(AsyncWebsocketConsumer):
 
@@ -27,6 +27,14 @@ class StockConsumer(AsyncWebsocketConsumer):
             task = PeriodicTask.objects.create(interval=schedule, name='every-10-seconds',
                                                task="mainWidget.tasks.update_stock", args=json.dumps([stockpicker]))
 
+    @sync_to_async
+    def addToStockDetail(self, stockpicker):
+        user = self.scope["user"]
+        for i in stockpicker:
+            stock, created = StockDetail.objects.get_or_create(stock=i)
+            stock.user.add(user)
+
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'stock_%s' % self.room_name
@@ -47,9 +55,32 @@ class StockConsumer(AsyncWebsocketConsumer):
         # add to Celery Beat
         await self.addToCeleryBeat(stockpicker)
 
+        await self.addToStockDetail(stockpicker)
 
+    @sync_to_async
+    def helper_func(self):
+        user = self.scope["user"]
+        stocks = StockDetail.objects.filter(user__id = user.id)
+        task = PeriodicTask.objects.get(name = "every-10-seconds")
+        args = json.loads(task.args)
+        args = args[0]
+        for i in stocks:
+            i.user.remove(user)
+            if i.user.count() == 0:
+                args.remove(i.stock)
+                i.delete()
+        if args == None:
+            args = []
+
+        if len(args) == 0:
+            task.delete()
+        else:
+            task.args = json.dumps([args])
+            task.save()
 
     async def disconnect(self, close_code):
+        await self.helper_func()
+
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -70,9 +101,24 @@ class StockConsumer(AsyncWebsocketConsumer):
             }
         )
 
+    @sync_to_async
+    def selectUserStocks(self):
+        user = self.scope["user"]
+        user_stocks = user.stockdetail_set.values_list('stock', flat = True)
+        return list(user_stocks)
+
     # Receive message from room group
     async def send_stock_update(self, event):
         message = event['message']
+        message = copy.copy(message)
 
+        user_stocks = await self.selectUserStocks()
+
+        keys = message.keys()
+        for key in list(keys):
+            if key in user_stocks:
+                pass
+            else:
+                del message[key]
         # Send message to WebSocket
         await self.send(text_data=json.dumps(message))
